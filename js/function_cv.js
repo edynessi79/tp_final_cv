71fa352a52241419dcbc7c182ccda79664be45c5
edynessi79 <edynessi@gmail.com>
2023-02-17 12:36:09 -0300

    tp_final_v1

---
+++ js/function_cv.js
@@ -0,0 +1,9 @@
+document.getElementById("boton_default").addEventListener('click',function(){
+    document.body.style.backgroundImage = 'url("C:/Users/enessi/Downloads/20200207115132-senderismo-en-chamonix-macizo-del-mont-blanc.jpg")';
+    document.body.style.backgroundAttachment = 'fixed';
+});
+
+document.getElementById("boton_color").addEventListener('click',function(){
+    document.body.style.background = 'red';
+});
+
